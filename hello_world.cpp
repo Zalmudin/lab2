a377c6b4ff48eeb9e298ed26613efbee1a175c3d
Zalmudin <d.zalma@mail.ru>
2025-06-01 22:29:37 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ int main() {
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    std::cout << "Hello, world from " << name << "!";
+    std::cout << "Hello, world from " << name << "!"; //c
     return 0;
 }
